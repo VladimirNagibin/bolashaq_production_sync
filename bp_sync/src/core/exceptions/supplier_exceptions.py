6b5a6125bc9c18363393ae91b48568de2eb7799a
Vladimir Nagibin <vlnagibin@yandex.ru>
2026-03-12 04:16:28 +0700

    Upd load supplier products

---
+++ bp_sync/src/core/exceptions/supplier_exceptions.py
@@ -0,0 +1,14 @@
+class ImportError(Exception):
+    pass
+
+
+class FileProcessingError(Exception):
+    pass
+
+
+class DataMappingError(Exception):
+    pass
+
+
+class DatabaseError(Exception):
+    pass
