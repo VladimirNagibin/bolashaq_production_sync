ee3f1c9bb7c8f73bc5917726296ba473eb167b06
Vladimir Nagibin <vlnagibin@yandex.ru>
2026-04-08 17:34:23 +0700

    Fix empty canegory

--- bp_sync/src/api/v1/suppliers/product_review.py
+++ bp_sync/src/api/v1/suppliers/product_review.py
@@ -151,7 +151,6 @@ async def review_product(
                 review_complex_data
             )
         )
-
         # Обработка ошибок
         error_message = None
         if request.query_params.get("error") == "name_required":
